88017bf026d2ba4127aea378713037f88cf58ad0
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2016-01-20 12:13:48 +0000

    cleaned code = GetClasspathRootTask already returns the absolute path, so I did not need to create another file and call getAbsolutePath()

--- core/src/core/annotations/configuration/tasks/GetAnnotationsFileStreamTask.java
+++ core/src/core/annotations/configuration/tasks/GetAnnotationsFileStreamTask.java
@@ -11,7 +11,7 @@ public class GetAnnotationsFileStreamTask implements OrionTask
     public InputStream run(AnnotationsConfigurationService annotationsConfigurationService, GetClasspathRootPathTask getClasspathRootTask, String libraryAnnotationsFilePath, String libraryName)
     {
         StringBuilder sb = new StringBuilder();
-        sb.append(new File((String)getClasspathRootTask.run(libraryName)).getAbsolutePath());
+        sb.append((String)getClasspathRootTask.run(libraryName));
         sb.append(libraryAnnotationsFilePath);
         return annotationsConfigurationService.getFileStream(sb.toString());
     }
