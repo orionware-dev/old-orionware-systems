8558bc9883d9cf9f7997cf61c7cc64cb0c98ec35
Dimitrios Efthymiou <dimitriosefthymiou@dimitrios.local>
2016-05-24 13:59:18 +0100

    created service task

--- core/coreannotationsservices/src/core/annotations/services/loader/AnnotationsLoaderServiceImpl.java
+++ core/coreannotationsservices/src/core/annotations/services/loader/AnnotationsLoaderServiceImpl.java
@@ -2,23 +2,26 @@ package core.annotations.services.loader;
 
 import java.io.InputStream;
 import core.annotations.services.AnnotationServiceObject;
+import core.annotations.services.loader.tasks.GetAnnotationsDefinitionFileStreamTask;
 import core.filesystem.streams.FileSystemService;
 import core.filesystem.streams.FileSystemServiceImpl;
 
 public class AnnotationsLoaderServiceImpl extends AnnotationServiceObject implements AnnotationsLoaderService
 {
     private FileSystemService fileSystemService;
+    private GetAnnotationsDefinitionFileStreamTask getAnnotationsDefinitionFileStreamTask;
     
     
     public AnnotationsLoaderServiceImpl()
     {
         this.fileSystemService = new FileSystemServiceImpl();
+        this.getAnnotationsDefinitionFileStreamTask = new GetAnnotationsDefinitionFileStreamTask();
     }
     
     
     @Override
     public InputStream getAnnotationsDefinitionFileStream(String libraryAnnotationsDefinitionFilePath)
     {
-        return fileSystemService.getFileStream(libraryAnnotationsDefinitionFilePath);
+        return getAnnotationsDefinitionFileStreamTask.run(fileSystemService, libraryAnnotationsDefinitionFilePath);
     }
 }
