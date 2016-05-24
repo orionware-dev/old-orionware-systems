8b7d14199a4cc39b3f5f7377f076ae6e3a8d7db0
Dimitrios Efthymiou <dimitriosefthymiou@dimitrios.local>
2016-05-24 15:02:30 +0100

    orged imports

--- core/coreannotationsservices/src/core/annotations/services/gathering/tasks/functions/ExtractMethodAnnotationFunction.java
+++ core/coreannotationsservices/src/core/annotations/services/gathering/tasks/functions/ExtractMethodAnnotationFunction.java
@@ -2,8 +2,6 @@ package core.annotations.services.gathering.tasks.functions;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import core.annotations.AnnotationTask;
-import core.annotations.services.AnnotationServiceObject;
 import core.functions.OrionAbstractFunction;
 
 public class ExtractMethodAnnotationFunction extends OrionAbstractFunction
