d677345b19bee29e8bf9dc5c9509afab8ef39c2a
Efthymiou Dimitrios <efthymiou.dimitrios1@gmail.com>
2016-06-05 18:53:43 +0100

    added exception message

--- core/coreannotationsservices/src/core/annotations/services/processor/impl/tasks/ApplyAnnotationToMethodTask.java
+++ core/coreannotationsservices/src/core/annotations/services/processor/impl/tasks/ApplyAnnotationToMethodTask.java
@@ -10,16 +10,19 @@ public class ApplyAnnotationToMethodTask extends AnnotationServiceObject impleme
 {
     public void run(ReflectionLoaderFacade reflectionLoaderFacade, Object object, OrionAnnotation registeredAnnotation)
     {
+        //instantiate annotation service
+        Object someAnnotationService = reflectionLoaderFacade.loadAndInstantiateClass(registeredAnnotation.getAnnotationService());
+        
         try
         {
-            //instantiate annotation service
-            Object someAnnotationService = reflectionLoaderFacade.loadAndInstantiateClass(registeredAnnotation.getAnnotationService());
             //call annotation service method that will process this annotation
-            Method someMethod = someAnnotationService.getClass().getMethod(registeredAnnotation.getAnnotationServiceMethodToCall(), Object.class);
+            Method someMethod = someAnnotationService.getClass()
+                                    .getMethod(registeredAnnotation.getAnnotationServiceMethodToCall(), Object.class);
             reflectionLoaderFacade.callMethod(someMethod, someAnnotationService, object);
         }
         catch(NoSuchMethodException exception)
         {
+            System.out.println("Method " + registeredAnnotation.getAnnotationServiceMethodToCall() + " does not exist");
             exception.printStackTrace();
         }
         catch(SecurityException exception)
