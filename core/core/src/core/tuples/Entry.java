07ee63bfb79936d7dbb81c7f2ee51630c585ff28
Efthymiou Dimitrios <efthymiou.dimitrios1@gmail.com>
2016-06-08 19:17:56 +0100

    created Entry class that represents a key-value pair

---
+++ core/core/src/core/tuples/Entry.java
@@ -0,0 +1,44 @@
+package core.tuples;
+
+public class Entry<T1, T2> extends KeyValuePair<T1, T2>
+{
+    private Object key;
+    private Object value;
+    
+    
+    public Entry()
+    {
+        
+    }
+    
+    
+    public Entry(Object key, Object value)
+    {
+        setKey(key);
+        setValue(value);
+    }
+
+
+    public Object getKey()
+    {
+        return this.key;
+    }
+
+
+    public void setKey(Object key)
+    {
+        this.key = key;
+    }
+
+
+    public Object getValue()
+    {
+        return this.value;
+    }
+
+
+    public void setValue(Object value)
+    {
+        this.value = value;
+    }
+}
