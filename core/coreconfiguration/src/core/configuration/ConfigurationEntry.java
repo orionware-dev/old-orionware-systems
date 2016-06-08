edbae7512315a108d2e54f4f21a003f4aa2d0901
Efthymiou Dimitrios <efthymiou.dimitrios1@gmail.com>
2016-06-08 19:19:56 +0100

    created ConfigurationEntry that represents an Entry

---
+++ core/coreconfiguration/src/core/configuration/ConfigurationEntry.java
@@ -0,0 +1,18 @@
+package core.configuration;
+
+import core.tuples.Entry;
+
+public class ConfigurationEntry<T1, T2> extends Entry<T1, T2>
+{
+    public ConfigurationEntry()
+    {
+        
+    }
+    
+    
+    public ConfigurationEntry(Object key, Object value)
+    {
+        setKey(key);
+        setValue(value);
+    }
+}
