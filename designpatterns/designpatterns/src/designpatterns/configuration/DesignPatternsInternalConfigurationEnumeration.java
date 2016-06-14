117898a2b668721dbbcee15a9f0f8a2b6784ebfb
Efthymiou Dimitrios <efthymiou.dimitrios1@gmail.com>
2016-06-14 22:34:48 +0100

    reformatted

--- designpatterns/designpatterns/src/designpatterns/configuration/DesignPatternsInternalConfigurationEnumeration.java
+++ designpatterns/designpatterns/src/designpatterns/configuration/DesignPatternsInternalConfigurationEnumeration.java
@@ -5,7 +5,8 @@ import core.configuration.DefaultConfigurationEnumeration;
 
 public enum DesignPatternsInternalConfigurationEnumeration implements ConfigurationEnumeration
 {
-    PIPELINE_CONFIGURATION_FILE_NAME("Pipeline.prop"), PIPELINE_CONFIGURATION_FILE_PATH("" + DesignPatternsConfigurationEnumeration.LIBRARY_CLASS_PATH.get() + "/" + DefaultConfigurationEnumeration.DEFAULT_CONFIGURATION_DIR.get() + "/" + PIPELINE_CONFIGURATION_FILE_NAME.get());
+    PIPELINE_CONFIGURATION_FILE_NAME("Pipeline.prop"),
+    PIPELINE_CONFIGURATION_FILE_PATH("" + DesignPatternsConfigurationEnumeration.LIBRARY_CLASS_PATH.get() + "/" + DefaultConfigurationEnumeration.DEFAULT_CONFIGURATION_DIR.get() + "/" + PIPELINE_CONFIGURATION_FILE_NAME.get());
 
     private final String dataStructuresConfigurationValue;
 
