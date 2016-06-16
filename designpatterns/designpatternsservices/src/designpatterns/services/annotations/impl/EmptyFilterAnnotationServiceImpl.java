544f18301aa363e868685fc583f758a75ef7b888
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2016-06-16 23:46:19 +0100

    fixed bug and created task

--- designpatterns/designpatternsservices/src/designpatterns/services/annotations/impl/EmptyFilterAnnotationServiceImpl.java
+++ designpatterns/designpatternsservices/src/designpatterns/services/annotations/impl/EmptyFilterAnnotationServiceImpl.java
@@ -9,14 +9,12 @@ import designpatterns.annotations.EmptyFilter;
 import designpatterns.pipeline.AbstractFilter;
 import designpatterns.services.DesignPatternsServicesObject;
 import designpatterns.services.annotations.EmptyFilterAnnotationService;
+import designpatterns.services.annotations.impl.tasks.ProcessEmptyFilterAnnotationTask;
 import designpatterns.services.pipeline.PipelineFilterService;
 
 public class EmptyFilterAnnotationServiceImpl extends DesignPatternsServicesObject implements EmptyFilterAnnotationService
 {
-    private PipelineFilterService pipelineFilterService;
-    private ReflectionLoaderFacade reflectionLoaderFacade;
-    private AnnotationsGatheringFacade annotationsGatheringFacade;
-    private Object object;
+    private ProcessEmptyFilterAnnotationTask processEmptyFilterAnnotationTask;
     
     
     public EmptyFilterAnnotationServiceImpl()
@@ -28,41 +26,13 @@ public class EmptyFilterAnnotationServiceImpl extends DesignPatternsServicesObje
     @Override
     public void processEmptyFilterAnnotation(Object object)
     {
-        this.object = object;
-        Arrays.stream(reflectionLoaderFacade.getMethodsArray(object)).forEach(method -> processMethodForEmptyFilterInjection(method));
+        processEmptyFilterAnnotationTask.run(object);
     }
     
     
-    private void processMethodForEmptyFilterInjection(Method method)
+    @Injector(ID = "designpatterns.services.annotations.impl.tasks.ProcessEmptyFilterAnnotationTask")
+    private void setProcessEmptyFilterAnnotationTask(ProcessEmptyFilterAnnotationTask processEmptyFilterAnnotationTask)
     {
-        reflectionLoaderFacade.makeMethodAccessible(method);
-        EmptyFilter emptyFilterAnnotation = (EmptyFilter)annotationsGatheringFacade.extractAnnotationFromMethod(method, EmptyFilter.class);
-        
-        if(emptyFilterAnnotation != null)
-        {
-            AbstractFilter emptyFilter = pipelineFilterService.createEmptyFilter();
-            reflectionLoaderFacade.callMethod(method, object, emptyFilter);
-        }
-    }
-
-
-    @Injector(ID = "designpatterns.services.pipeline.impl.PipelineFilterServiceImpl")
-    private void setPipelineFilterService(PipelineFilterService pipelineFilterService)
-    {
-        this.pipelineFilterService = pipelineFilterService;
-    }
-    
-    
-    @Injector(ID = "core.reflection.facades.loader.impl.ReflectionLoaderFacadeImpl")
-    private void setReflectionLoaderFacade(ReflectionLoaderFacade reflectionLoaderFacade)
-    {
-        this.reflectionLoaderFacade = reflectionLoaderFacade;
-    }
-    
-    
-    @Injector(ID = "core.annotations.facades.gathering.impl.AnnotationsGatheringFacadeImpl")
-    private void setAnnotationsGatheringFacade(AnnotationsGatheringFacade annotationsGatheringFacade)
-    {
-        this.annotationsGatheringFacade = annotationsGatheringFacade;
+        this.processEmptyFilterAnnotationTask = processEmptyFilterAnnotationTask;
     }
 }
